142851b1689e27f4033877b5a787c5292783fb82
José Páez <jose.paez@gmail.com>
2025-10-17 20:33:40 -0300

    Añadidos comentarios sobre tipos y clases parciales
    Se añadieron comentarios explicativos en `_05_TiposAnonimos.cs`
    sobre los tipos anónimos, incluyendo sus características y uso.
    
    En `_06_ClasesYMetodosParciales.cs`, se documentaron las clases
    parciales y métodos parciales, explicando su funcionalidad y
    cómo se implementan. No se realizaron cambios en la lógica del
    código, solo se agregó documentación para mejorar la claridad.

--- 01/m01/_05_TiposAnonimos.cs
+++ 01/m01/_05_TiposAnonimos.cs
@@ -1,5 +1,12 @@
 ﻿namespace m01
 {
+	// TIPOS ANÓNIMOS:
+	// Los tipos anónimos permiten crear objetos sin definir clases explícitas, encapsulando propiedades temporalmente.
+	// Sus características incluyen:
+	//	- Definición rápida: Usan var y una lista de propiedades entre { }.
+	//	- Inmutabilidad: Sus propiedades son de solo lectura.
+	//	- Tipado fuerte: El compilador infiere su tipo en tiempo de compilación.
+
 	public class TiposAnonimos
 	{
 	}
--- 01/m01/_06_ClasesYMetodosParciales.cs
+++ 01/m01/_06_ClasesYMetodosParciales.cs
@@ -1,6 +1,12 @@
 ﻿
 namespace m01
 {
+	// CLASES PARCIALES:
+	// Las clases parciales, definidas con la palabra clave 'partial', permiten dividir una clase en varios archivos dentro del mismo ensamblado y espacio de nombres. El compilador las combina en una única clase al compilar.
+
+	// MÉTODOS PARCIALES:
+	// Los métodos parciales, definidos con la palabra clave partial, permiten que una clase parcial tenga métodos opcionales. Si no se implementan, el compilador elimina sus llamadas sin impactar el rendimiento.
+
 	public class ClasesYMetodosParciales
 	{
 
